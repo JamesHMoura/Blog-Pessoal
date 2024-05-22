edc18eff7898640c3e16b434e7af341e3fa27510
James Moura <jheyharrison@gmail.com>
2024-05-21 22:19:34 -0300

    Criação do projeto BlogPessoal!

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,67 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "o atributo título é obrigatorio")
+	@Size(min =5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo TEXTO é obrigatório")
+	@Size(min = 10 , max = 1000, message = "O atributo título deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+
+	@UpdateTimestamp
+	private LocalDateTime time;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+	
+	
+}
