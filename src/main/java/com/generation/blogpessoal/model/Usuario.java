474c8618fd55cddbfef96fcc77fa98ad7e00107b
James Moura <jheyharrison@gmail.com>
2024-06-06 14:49:28 -0300

    UsuarioControllerTest adicionado!

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -43,53 +43,91 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	
+	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	
+	public Usuario() {
+		
+	}
+
 	public Long getId() {
 		return id;
 	}
+
+
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+
+
 	public String getNome() {
 		return nome;
 	}
+
+
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+
+
 	public String getUsuario() {
 		return usuario;
 	}
+
+
 
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
 
+
+
 	public String getSenha() {
 		return senha;
 	}
+
+
 
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
+
+
 	public String getFoto() {
 		return foto;
 	}
+
+
 
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
 
+
+
 	public List<Postagem> getPostagem() {
 		return postagem;
 	}
 
+
+
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
+
 	
 	
 }
